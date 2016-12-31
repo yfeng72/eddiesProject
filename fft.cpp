930b1a455c864e89f001ef63566dcd55ec0f95a6
eddiewu19 <eddiewu0429@gmail.com>
2016-12-30 19:42:32 -0800

    follow bill
    follow bill

--- fft.cpp
+++ fft.cpp
@@ -10,26 +10,31 @@ vector<complex> fft::ditfft2(vector<complex> x, int N) {
     return ret;
 }
 
-void fft::recursivefft(vector<complex> &x, int N, vector<complex> prevlist){
+void fft::recursivefft(vector<complex> &x, int N, vector<complex> prevlist, int curr){
     if(N==1){
         return;
     }
-    int halflength=N/2;
+    int halflength=curr/2;
     vector<complex> firsthalf;
     vector<complex> secondhalf;
 
     //initialization of the recursive vector
     for(int i=0; i<halflength; i++){
         complex t1(x[2*i], x[2*i]);
-        complex t2(x[2*i-1], x[2*i-1]);
+        complex t2(x[2*i+1], x[2*i+1]);
         firsthalf.push_back(t1);
         secondhalf.push_back(t2);
     }
-    
-    recursivefft(firsthalf, halflength, prevlist);
-    recursivefft(secondhalf, halflength, prevlist);
+
+    recursivefft(firsthalf, N, prevlist, halflength);
+    recursivefft(secondhalf, N, prevlist,halflength);
     for(int i=0; i<halflength; i++){
         complex temp=firsthalf[i];
+        complex product=secondhalf[i]*prevlist[(N/curr)*i];
+        complex sum=temp+product;
+        complex diff=temp-product;
+        prevlist[i]=sum;
+        prevlist[halflength+i] = diff;
 
     }
 
