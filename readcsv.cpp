e4ea3401caf2ecfa359f98556c131726781f4495
eddiewu19 <eddiewu0429@gmail.com>
2017-01-09 09:22:00 -0800

    READCSV
    Haven't tested

---
+++ readcsv.cpp
@@ -0,0 +1,34 @@
+//
+// Created by sheny on 2017/1/2.
+//
+
+#include "readcsv.h"
+
+readcsv::readcsv(string csvname) {
+    string line;
+    ifstream myfile(csvname);
+    int i=0;
+    if(myfile.is_open()){
+        complex *newcomplex=new complex;
+        while(getline(myfile, line,',')){
+            //cout << line << endl;
+            if(i%2==0){
+                newcomplex->re=line;
+            }
+            else{
+                newcomplex->im=line;
+            }
+            i++;
+            if(i==2){
+                allcomplex.push_back(*newcomplex);
+                i=0;
+                complex *newcomplex=new complex;
+            }
+        }
+        myfile.close();
+    }
+    else{
+        cout << "Unable to open the file" << endl;
+    }
+
+}
---
+++ readcsv.h
@@ -0,0 +1,19 @@
+//
+// Created by sheny on 2017/1/2.
+//
+#include "complex.h"
+#include <fstream>
+#include <vector>
+#ifndef EDDIESPROJECT_READCSV_H
+#define EDDIESPROJECT_READCSV_H
+
+
+class readcsv {
+    public:
+        readcsv(string csvname);
+        vector<complex> allcomplex;
+
+};
+
+
+#endif //EDDIESPROJECT_READCSV_H
