81e63958680cfec151d27dbea31afd2402b295c6
Bill Feng <fengy@gatech.edu>
2017-01-09 22:34:11 -0500

    Fixed multithreaded implementation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "complex.h"
 #include "fft.h"
+#include "threadedFFT.h"
 #include <cmath>
 
 int main() {
@@ -14,10 +15,18 @@ int main() {
     list.push_back( complex( 6.0, 0.0 ) );
     list.push_back( complex( 7.0, 0.0 ) );
     list.push_back( complex( 8.0, 0.0 ) );
-    fft transform( 8, false, NULL );
+    fft transform( 8, false, list );
     vector<complex> transformedList = transform.ditfft2( list, 8 );
     std::cout << "Scaled Transformed List:" << std::endl;
     for ( complex item : transformedList ) {
+        std::cout << item.re / sqrt( 8.0 ) << " + " << item.im / sqrt( 8.0 ) << "i" << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+    threadedFFT threaded( 8 );
+    threaded.run( list );
+    vector<complex> threadedTransform = threaded.transformedList;
+    std::cout << "Scaled Transformed List:" << std::endl;
+    for ( complex item : threadedTransform ) {
         std::cout << item.re / sqrt( 8.0 ) << " + " << item.im / sqrt( 8.0 ) << "i" << std::endl;
     }
     return 0;
--- readcsv.cpp
+++ readcsv.cpp
@@ -4,31 +4,30 @@
 
 #include "readcsv.h"
 
-readcsv::readcsv(string csvname) {
+readcsv::readcsv( string csvname ) {
     string line;
-    ifstream myfile(csvname);
-    int i=0;
-    if(myfile.is_open()){
-        complex *newcomplex=new complex;
-        while(getline(myfile, line){
-            while (getline(myfile,line,',')) {
+    ifstream myfile( csvname );
+    int i = 0;
+    if ( myfile.is_open()) {
+        complex *newcomplex = new complex;
+        while ( getline( myfile, line )) {
+            while ( getline( myfile, line, ',' )) {
                 //cout << line << endl;
-                if (i % 2 == 0) {
-                    newcomplex->re = (int) line;
+                if ( i % 2 == 0 ) {
+                    newcomplex->re = std::stod( line );
                 } else {
-                    newcomplex->im = (int) line;
+                    newcomplex->im = std::stod( line );
                 }
                 i++;
-                if (i == 2) {
-                    allcomplex.push_back(*newcomplex);
+                if ( i == 2 ) {
+                    allcomplex.push_back( *newcomplex );
                     i = 0;
                     complex *newcomplex = new complex;
                 }
             }
         }
         myfile.close();
-    }
-    else{
+    } else {
         cout << "Unable to open the file" << endl;
     }
 
